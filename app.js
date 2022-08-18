d81841fd27992b415d7091607165be5eb3270b51
songhwa-ryu <rsh120@daum.net>
2022-08-18 15:41:48 +0900

    3commit

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const { Client } = require('pg')
-const dbInfo = require('../DB/dbInfo')
+const dbInfo = { } // require('../DB/dbInfo')
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
+app.get('/', (req,res)=>{
+    res.send('Success')
+})
 
 app.get('/kor',(req, res)=>{
     res.send('안녕하세요')
